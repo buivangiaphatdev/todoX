cd50eb13376bcf70068f4d330dc94f0a60bd4d1b
buivangiaphatdev <buivangiaphat@gmail.com>
2025-10-08 03:16:43 +0700

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,34 +3,34 @@ import taskRoute from "./routes/tasksRouters.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
-
 import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
-const _dirname = path.resolve();
+const __dirname = path.resolve();
 
 const app = express();
 
+// middlewares
 app.use(express.json());
 
-if (provess.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production") {
   app.use(cors({ origin: "http://localhost:5173" }));
 }
 
 app.use("/api/tasks", taskRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(_dirname, "../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+    console.log(`server bắt đầu trên cổng ${PORT}`);
   });
 });
