6cc5c76b23c42cd30c2d3abe32c9b0c1dcc09c90
buivangiaphatdev <buivangiaphat@gmail.com>
2025-10-08 03:07:38 +0700

    chuẩn bị deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,31 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
 
+import path from "path";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const _dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+
+if (provess.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
+
 app.use("/api/tasks", taskRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "../frontend/dist/index.html"));
+  });
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
